0f5c5433e6daba3a14527f252b7a69500c88e821
Felipe Cimardi Macedo <felipecimardimacedo@gmail.com>
2022-01-17 22:29:11 -0300

    Up project to GitHub

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Balta.ContentContext;
+
+namespace Balta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var course = new Course();
+            var career = new Career();
+
+            career.Items.Add(new CareerItem());
+            Console.WriteLine(career.TotalCourses);
+        }
+    }
+}
